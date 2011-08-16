6964f8d3d50cd14f40afbaf4dea88685346e1e31
Lee Gao <leeyuan123@yahoo.com>
2011-08-16 18:06:19 -0500

    Edited lua.js via GitHub

--- lua.js
+++ lua.js
@@ -326,7 +326,7 @@ var LuaHighlightRules = function() {
             regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
         }, {
             token : "keyword.operator",
-            regex : "\\+|\\-|\\*|\\/|%|\\#|\\^|~|<|>|<=|=>|==|~=|=|\\:"
+            regex : "\\+|\\-|\\*|\\/|%|\\#|\\^|~|<|>|<=|=>|==|~=|=|\\:|\\.\\.\\.|\\.\\."
         }, {
             token : "lparen",
             regex : "[\\[\\(\\{]"
